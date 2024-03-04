fdf46717375a27c243049859ebc9d79365122102
Dmorim <92826578+Dmorim@users.noreply.github.com>
2024-03-04 09:05:23 -0300

    Update Consultas_Dist_Saldo_List.py

--- Consultas/Consultas_Dist_Saldo_List.py
+++ Consultas/Consultas_Dist_Saldo_List.py
@@ -29,18 +29,6 @@ def List_Treeview_Screen(self, parent):
     
     Treeview_Insert(treeview, self.dist_saldo_list)
     
-def Treeview_Select(self, treeview):
-    from Banco_de_Dados.Inventario_Conn import Connect
-    self.dist_saldo_list = []
-    query = "select l.cdpro, p.nmpro, sum(iif(l.TPMOV = 'S', l.quant, -l.quant)) as saldo_lan, p.saldo as saldo_pro, iif(sum(iif(l.TPMOV = 'S', l.quant, -l.quant)) <> p.saldo, 'S', 'N') as distorcao  from in01lan l left join in01pro p on l.cdpro = p.cdpro where p.classificacao_produto in ('00','01', '02', '03', '04', '05', '06') group by l.cdpro, p.saldo, p.nmpro"
-    Connect.cursor.execute(query)
-    rows = Connect.cursor.fetchall()
-    for row in rows:
-        cdpro, nmpro, saldo_lan, saldo_pro, distorcao = row
-        if distorcao == 'S':
-            self.dist_saldo_list.append([cdpro, nmpro, saldo_lan, saldo_pro])
-    Treeview_Insert(treeview, self.dist_saldo_list)
-    
 def Treeview_Insert(treeview, rows):
     for row in rows:
         treeview.insert('', 'end', values= row)
